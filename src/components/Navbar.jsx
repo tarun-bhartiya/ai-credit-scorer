dcb7e018069986e06abf040f2ec8ca1e511cbc7f
Tarun Bhartiya <tbhartiya@snapfinancebpo.com>
2025-09-10 17:06:56 +0530

    remove unused files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,18 @@
-import * as React from "react";
 import PropTypes from "prop-types";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import { Link, Outlet } from "react-router";
+import { Link } from "react-router";
 
 const navItems = [
   { id: 1, label: "Merchants", link: "/" },
   { id: 2, label: "Consumers", link: "/consumers" },
 ];
 
-function NavBar(props) {
+function NavBar() {
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
